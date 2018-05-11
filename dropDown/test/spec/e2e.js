6ba59fbc20cecc2074997f9142f3fad5ee27a303
rough33 <stevenlt4@ufl.edu>
2018-05-11 23:07:36 +0800

    add spec file

---
+++ dropDown/test/spec/e2e.js
@@ -0,0 +1,19 @@
+describe("dropDown e2e test", function() {
+
+it('Go to main page', function() {
+  browser.driver.ignoreSynchronization = true
+  //need to change path for test
+  browser.driver.get('file:///Users/weit/Desktop/dropdownJqBt.html');
+  expect(browser.driver.getCurrentUrl()).toContain('dropdown')
+});
+
+it('click dropdown choose item and show related link', function() {
+  browser.driver.findElement(by.className('dropbtn')).click();
+  browser.driver.findElement(by.id('myInput')).sendKeys('Nanj');
+  browser.driver.findElement(by.partialLinkText('Nanjing')).click()
+  //for record purpose
+  browser.driver.sleep(2000);
+  expect(browser.driver.getCurrentUrl()).toContain('#Nanjing')
+});
+
+});
